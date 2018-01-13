176fcde1ddf7c8482e4e4d75c6521687d23ec5df
vipulkrg@gmail.com <vipulkrg@gmail.com>
2018-01-13 18:40:59 +0000

    automatically backed up by learn

--- closure.js
+++ closure.js
@@ -2,7 +2,7 @@ function parent() {
   var innerVariable = "Go to your room!";
   // console.log(inaccessible)
   return function childe() {
-    var inaccessible = "**sneaks out the bedroom window**;
+    var inaccessible = "**sneaks out the bedroom window**";
     return innerVariable;
   }
 }
